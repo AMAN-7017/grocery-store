714ed27ddf8cc54c2079be269430155c91726ada
AMAN-7017 <mohdaman1011@gmail.com>
2025-01-14 00:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -144,8 +144,10 @@ def logout():
 header = {'content-type':'application/json','Accept':'text/html'}
 # cate_api_url = "http://127.0.0.1:5000/api/category"
 # prod_api_url = "http://127.0.0.1:5000/api/product"
-cate_api_url = "https://grocery-store-ujwm.onrender.com/api/category"
-prod_api_url = "https://grocery-store-ujwm.onrender.com/api/product"
+# cate_api_url = "https://grocery-store-ujwm.onrender.com/api/category"
+# prod_api_url = "https://grocery-store-ujwm.onrender.com/api/product"
+cate_api_url = "https://grocery-store-mauve.vercel.app/api/category"
+prod_api_url = "https://grocery-store-mauve.vercel.app/api/product"
 
 # ----------------------------Admin Home-------------------------------
 
